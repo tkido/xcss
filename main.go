db7628518abaf27181d7474768f0c2de096b486d
tkido <takanorikido@gmail.com>
2016-12-06 11:32:28 +0900

    Use standard log package

--- main.go
+++ main.go
@@ -3,65 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/xml"
-	"io"
+	"log"
 	"os"
-
-	"github.com/tkido/gotools/log"
+	"time"
 )
-
-type Tag struct {
-	Name     xml.Name
-	Attr     []xml.Attr
-	Children []interface{}
-}
-
-func (t *Tag) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	start.Name = t.Name
-	start.Attr = t.Attr
-	e.EncodeToken(start)
-	for _, v := range t.Children {
-		switch v.(type) {
-		case *Tag:
-			child := v.(*Tag)
-			if err := e.Encode(child); err != nil {
-				return err
-			}
-		case xml.CharData:
-			e.EncodeToken(v.(xml.CharData))
-		case xml.Comment:
-			e.EncodeToken(v.(xml.Comment))
-		}
-	}
-	e.EncodeToken(start.End())
-	return nil
-}
-
-func (t *Tag) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	t.Name = start.Name
-	t.Attr = start.Attr
-	for {
-		token, err := d.Token()
-		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		}
-		switch token.(type) {
-		case xml.StartElement:
-			tok := token.(xml.StartElement)
-			var data *Tag
-			if err := d.DecodeElement(&data, &tok); err != nil {
-				return err
-			}
-			t.Children = append(t.Children, data)
-		case xml.CharData:
-			t.Children = append(t.Children, token.(xml.CharData).Copy())
-		case xml.Comment:
-			t.Children = append(t.Children, token.(xml.Comment).Copy())
-		}
-	}
-}
 
 const s = `<styles>
 	<item type="image" id="background" w="800" h="416" imageurl="@DNA/common/bg.png"/>
@@ -69,14 +14,17 @@ const s = `<styles>
 `
 
 func main() {
-	defer log.Close()
+	f, err := os.Create(time.Now().Format("log/2006_0102_1504_05.log"))
+	if err != nil {
+		return
+	}
+	log.SetOutput(f)
 
 	v := &Tag{}
 	xml.NewDecoder(bytes.NewBuffer([]byte(s))).Decode(&v)
 
-	log.D(v.Name)
-	log.D(v.Attr)
-	log.D(v.Children)
+	log.Println(v.Name)
+	log.Println(v.Attr)
+	log.Println(v.Children)
 
-	log.D(xml.NewEncoder(os.Stdout).Encode(v))
 }
