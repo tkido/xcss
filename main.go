d4fa3b967c9967574bfc47d8024a8bbc8a448e06
tkido <takanorikido@gmail.com>
2016-12-08 21:15:39 +0900

    Init walk

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 var sets Settings
 
 func main() {
-	classes := []string{"bar", "foo", "hoge", "piyo"}
-	log.Println(comb(classes))
-
 	sets = Settings{}
-	readCSS("./testdata/platform/platform_css.xml")
-	readCSS("./testdata/platform/project/project_css.xml")
-	convCSS("./testdata/platform/project/apps/foo/foo_main.xml")
+	csss := []string{
+		"./testdata/platform/platform_css.xml",
+		"./testdata/platform/project/project_css.xml",
+	}
+	for _, css := range csss {
+		readCSS(css)
+	}
+
+	appsPath := "./testdata/platform/project/apps"
+
+	convCSS("./testdata/platform/project/apps/foo/foo_main_style.xml")
+
+	err := filepath.Walk(appsPath,
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				// 特定のディレクトリ以下を無視する場合は
+				// return filepath.SkipDir
+				return nil
+			}
+			rel, err := filepath.Rel(appsPath, path)
+			fmt.Println(rel)
+			return nil
+		})
+
+	if err != nil {
+		fmt.Println(1, err)
+	}
+
 }
